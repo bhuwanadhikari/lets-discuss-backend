ce66bdbc0c20fdd1233e0cd8ce2d0db99da94df6
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-08 11:51:14 +0545

    Revive the endpoints

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// const Post = require('./models/Post');
+const Post = require('./models/Post');
 
 
 const app = express();
@@ -12,63 +12,62 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// //database
-// const db = require('./config/keys').mongoURI;
+//database
+const db = require('./config/keys').mongoURI;
 
 
-// //connection to database
-// mongoose
-//     .connect(db, { useNewUrlParser: true,  })
-//     .then(() => console.log("Connected to the mongoose"))
-//     .catch(err => console.log(err));
-// mongoose.set('useFindAndModify', false);
+//connection to database
+mongoose
+    .connect(db, { useNewUrlParser: true,  })
+    .then(() => console.log("Connected to the mongoose"))
+    .catch(err => console.log(err));
+mongoose.set('useFindAndModify', false);
 
-//Homepage
 app.get('/', (req, res) => {
-    res.json({msg: "Success"});
+    res.json({msg: "Success"})
 });
 
-// //add posts
-// app.post('/add-post', (req, res) => {
-//     const newPost = new Post({
-//         postText: req.body.postText
-//     });
+//add posts
+app.post('/add-post', (req, res) => {
+    const newPost = new Post({
+        postText: req.body.postText
+    });
 
-//     // console.log(newPost);
-//     newPost.save()
-//     .then(post => res.json(post))
-//     .catch(err => console.log(err));
+    // console.log(newPost);
+    newPost.save()
+    .then(post => res.json(post))
+    .catch(err => console.log(err));
 
-// });
+});
 
-// //like post
-// app.post('/like', (req, res) => {
-//     // console.log(req.body);
-//     Post.findByIdAndUpdate(req.body.postId, {$inc:{likesCount: 1}})
-//     .then(post => post._id)
-//     .then(_id => Post.findById(_id))
-//     .then(post => res.json(post))
-//     .catch(err => console.log(err));
-// });
+//like post
+app.post('/like', (req, res) => {
+    // console.log(req.body);
+    Post.findByIdAndUpdate(req.body.postId, {$inc:{likesCount: 1}})
+    .then(post => post._id)
+    .then(_id => Post.findById(_id))
+    .then(post => res.json(post))
+    .catch(err => console.log(err));
+});
 
-// //comment on post
-// app.post('/comment', (req, res) => {
-//     console.log("")
-//     Post.findByIdAndUpdate(req.body.postId, {$push:{comments: req.body.commentValue}})
-//     .then(post => post._id)
-//     .then(_id => Post.findById(_id))
-//     .then(post => res.json(post))
-//     .catch(err => console.log(err));
-// });
+//comment on post
+app.post('/comment', (req, res) => {
+    console.log("")
+    Post.findByIdAndUpdate(req.body.postId, {$push:{comments: req.body.commentValue}})
+    .then(post => post._id)
+    .then(_id => Post.findById(_id))
+    .then(post => res.json(post))
+    .catch(err => console.log(err));
+});
 
-// //get all post
-// app.get('/get-all-posts', (req, res) => {
-//     Post.find()
-//     .then(allPosts => {
-//         res.json(allPosts);
-//     })
-//     .catch(err => console.log(err));
-// });
+//get all post
+app.get('/get-all-posts', (req, res) => {
+    Post.find()
+    .then(allPosts => {
+        res.json(allPosts);
+    })
+    .catch(err => console.log(err));
+});
 
 
 
