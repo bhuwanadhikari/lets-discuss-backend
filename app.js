34f43960ba45df14fdcff4b239261c74623eeedd
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-08 10:13:12 +0545

    Update app.js for deployment

--- app.js
+++ app.js
@@ -24,15 +24,7 @@ mongoose
 
 
 
-//serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 mongoose.set('useFindAndModify', false);
 
@@ -82,17 +74,6 @@ app.get('/get-all-posts', (req, res) => {
 
 
 
-
-
-
-
-
-
-
 const port = process.env.PORT || 5000;
-
-
-
-
 app.listen(port, () => console.log(`Server running in port ${port}`));
 
